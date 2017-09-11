1a25ffd45db142db9a573aa379770aa0dcd69a9f
dacup72 <dacup72@gmail.com>
2017-09-11 17:41:41 -0600

    updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
