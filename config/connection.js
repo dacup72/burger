4bc9ece7408bbef55653b2fbc7e234e716fc41c5
dacup72 <dacup72@gmail.com>
2018-03-27 11:15:03 -0600

    Fixing file structure

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     port: 3306,
-    host: "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "nzuz2x2or373zudh",
-    password: "obbayjx2k3ul4n46",
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
     database: "burgers_db"
   });
 };
