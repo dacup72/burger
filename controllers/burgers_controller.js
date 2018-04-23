9da790098570f2779a1b4c57c4629b0ea1f6592e
dacup72 <dacup72@gmail.com>
2018-04-23 12:20:11 -0600

    format

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,34 +3,36 @@ var express = require("express");
 var router = express.Router();
 var db = require('../models');
 
-router.get("/", function(req, res) {
-  res.redirect("/burgers");
+router.get("/", function (req, res) {
+	res.redirect("/burgers");
 });
 
-router.get("/burgers", function(req, res) {
+router.get("/burgers", function (req, res) {
 	db.Burger.findAll({
-    include: [db.Customer],
-    order: [
-      ["burger_name", "ASC"]
-    ]
-  }).then(function(dbBurger) {
-    // into the main index, updating the page
-      var hbsObject = {
-        burger: dbBurger
-      };
-      return res.render("index", hbsObject);
-    });
+		include: [
+			db.Customer
+		],
+		order: [
+			["burger_name", "ASC"]
+		]
+	}).then(function (dbBurger) {
+		// into the main index, updating the page
+		var hbsObject = {
+			burger: dbBurger
+		};
+		return res.render("index", hbsObject);
+	});
 })
 
 
-router.put("/burger/update", function(req, res) {
-	burger.update(req.body.burger_id, function(result) {
+router.put("/burger/update", function (req, res) {
+	burger.update(req.body.burger_id, function (result) {
 		res.redirect("/");
 	})
 });
 
-router.post("/burger/create", function(req, res) {
-	burger.create(req.body.burger_name, function(result) {
+router.post("/burger/create", function (req, res) {
+	burger.create(req.body.burger_name, function (result) {
 		res.redirect('/');
 	})
 })
