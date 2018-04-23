c0be4c6568f68580f9d308ca4917a3802c811b45
dacup72 <dacup72@gmail.com>
2018-04-23 09:06:51 -0600

    Working on controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,7 @@
-//import express
-
-//import burgers.js
-
-//create the router for the app and export the router
 
 var express = require("express");
 var router = express.Router();
-var burger = require('../models/burger.js')
+var db = require('../models');
 
 router.get("/", function(req, res) {
 	burger.all(function(burger_data) {
