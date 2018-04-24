91c340176c6e3839359d7f6e1a084530812fcdeb
dacup72 <dacup72@gmail.com>
2018-04-24 13:25:25 -0600

    fixing format

--- config/orm.js
+++ config/orm.js
@@ -8,29 +8,29 @@
 
 //export the orm object with module.exports
 
-var connection = require("../config/connection.js");
+// var connection = require("../config/connection.js");
 
-var orm = {
-  all: function(tableInput, cb){
-    connection.query("SELECT * FROM " +tableInput+";", function(error, result) {
-      if(error) throw error;
-      cb(result)
-    })
-  },
+// var orm = {
+//   all: function(tableInput, cb){
+//     connection.query("SELECT * FROM " +tableInput+";", function(error, result) {
+//       if(error) throw error;
+//       cb(result)
+//     })
+//   },
 
-  update: function(tableInput, condition, cb) {
-    connection.query("UPDATE "+tableInput+" SET devoured=true WHERE id="+condition+";", function(error, result){
-      if(error)throw error;
-      cb(result);
-    })
-  },
+//   update: function(tableInput, condition, cb) {
+//     connection.query("UPDATE "+tableInput+" SET devoured=true WHERE id="+condition+";", function(error, result){
+//       if(error)throw error;
+//       cb(result);
+//     })
+//   },
 
-  create: function(tableInput, val, cb) {
-    connection.query("INSERT INTO "+tableInput+" (burger_name)VALUES ('"+val+"');", function(error, result){
-      if(error) throw error;
-      cb(result);
-    })
-  }
-}
+//   create: function(tableInput, val, cb) {
+//     connection.query("INSERT INTO "+tableInput+" (burger_name)VALUES ('"+val+"');", function(error, result){
+//       if(error) throw error;
+//       cb(result);
+//     })
+//   }
+// }
 
-module.exports = orm;
+// module.exports = orm;
