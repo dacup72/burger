6d79b638f5be6a879f3ffd1490801a08adb10126
dacup72 <dacup72@gmail.com>
2018-04-26 11:04:58 -0600

    makeing post route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,6 +24,17 @@ router.get("/burgers", function (req, res) {
 	});
 })
 
+// post route to create burgers
+router.post("/burgers/create", function(req, res) {
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  })
+    .then(function(dbBurger) {
+      // console.log(dbBurger);
+      
+    });
+});
+
 
 router.put("/burger/update", function (req, res) {
 	burger.update(req.body.burger_id, function (result) {
@@ -31,10 +42,10 @@ router.put("/burger/update", function (req, res) {
 	})
 });
 
-router.post("/burger/create", function (req, res) {
-	burger.create(req.body.burger_name, function (result) {
-		res.redirect('/');
-	})
-})
+// router.post("/burger/create", function (req, res) {
+// 	burger.create(req.body.burger_name, function (result) {
+// 		res.redirect('/');
+// 	})
+// })
 
 module.exports = router;
